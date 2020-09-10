e08ccb48ba3d005b409ffdce46ab86ef64825b8f
Satyadeep <satyadeep.roat@gmail.com>
2020-09-10 22:20:30 +0530

    Updates

---
+++ precache-manifest.05992629748c84a534a54ff63ca0ea9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b72b51918736c934bd77e5bbcfb94788",
+    "url": "/canvas-board/index.html"
+  },
+  {
+    "revision": "a8d48f02ecf9a2419faf",
+    "url": "/canvas-board/static/css/main.8478e2c2.chunk.css"
+  },
+  {
+    "revision": "9a2492ff13702ff77190",
+    "url": "/canvas-board/static/js/2.857d4db6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvas-board/static/js/2.857d4db6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d48f02ecf9a2419faf",
+    "url": "/canvas-board/static/js/main.713283f1.chunk.js"
+  },
+  {
+    "revision": "709738cd9d5433db8135",
+    "url": "/canvas-board/static/js/runtime-main.1e370a3a.js"
+  }
+]);
