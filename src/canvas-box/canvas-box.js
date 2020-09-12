b06af7d271762e44641cd09d84142e2d58741ad5
Satyadeep <satyadeep.roat@gmail.com>
2020-09-12 16:30:46 +0530

    touch active

--- src/canvas-box/canvas-box.js
+++ src/canvas-box/canvas-box.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const CanvasBox = React.forwardRef((props, ref) => {
-  const {startDrawing, finishDrawing, draw} = props;
+  const { startDrawing, finishDrawing, draw } = props;
   return (
-    <div  className="canvas-box">
-      <canvas width={500} height={500} ref={ref}         onMouseDown={startDrawing}
+    <div className="canvas-box">
+      <canvas
+        width={500}
+        height={500}
+        ref={ref}
+        onTouchMove={draw}
+        onTouchStart={startDrawing}
+        onTouchEnd={finishDrawing}
+        onMouseDown={startDrawing}
         onMouseUp={finishDrawing}
         onMouseMove={draw}
-/>
+      />
     </div>
-  )
+  );
 });
 
 export default CanvasBox;
